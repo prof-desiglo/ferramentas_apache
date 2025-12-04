22abad113e71050f7622cf49ef9098447adaf608
Felipe Desiglo Ferrare <desiglo84@gmail.com>
2025-12-04 13:53:56 -0300

    Add files via upload

--- spark/setup-hdfs.sh
+++ spark/setup-hdfs.sh
@@ -1,11 +1,14 @@
 hdfs dfs -mkdir -p /data/results
-hdfs dfs -chmod 644 /data/results
 echo 'Criada pasta de resultados!'
-hdfs dfs -mkdir -p /user/hadoop
-echo 'Criada pasta do hadoop!'
-hdfs dfs -put /tmp/movies.csv /user/hadoop/
-hdfs dfs -chmod 644 /user/hadoop/movies.csv
+hdfs dfs -mkdir -p /user/spark
+echo 'Criada pasta do spark!'
+hdfs dfs -put /tmp/movies.csv /user/spark/
 echo 'Arquivo no HDFS:'
-hdfs dfs -ls /user/hadoop/
+hdfs dfs -ls /user/spark/
 echo -e '\nPrimeiras 5 linhas:'
-hdfs dfs -cat /user/hadoop/movies.csv | head -5
+hdfs dfs -cat /user/spark/movies.csv | head -5
+hdfs dfs -chown -R spark:spark /user/spark/
+hdfs dfs -chown -R spark:spark /data/results
+hdfs dfs -chmod -R 777 /data/results
+hdfs dfs -chmod 644 /user/spark/movies.csv
+echo 'Permissões corrigidas!'
